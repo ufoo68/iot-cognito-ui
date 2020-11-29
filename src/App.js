a962d07e28504a29da6a400f4c9b1dd8a8f8f78a
ufoo68 <mitarou.yuutarou@gmail.com>
2020-11-29 11:01:00 +0900

    Implemented!

--- src/App.js
+++ src/App.js
@@ -1,95 +1,57 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { PubSub } from 'aws-amplify'
 import { AWSIoTProvider } from '@aws-amplify/pubsub'
+import cryptoRandomString from 'crypto-random-string'
 
 Amplify.configure({
-    Auth: {
-        identityPoolId: 'ap-northeast-1:2045dcde-d565-4ed1-995c-8d43f52b8ab5', 
-        region: 'ap-northeast-1', 
-        userPoolId: 'ap-northeast-1_vSRFqM3Vd', 
-        userPoolWebClientId: '29adpeuu3v2aujm339cvtig436', 
-    }
+  Auth: {
+    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+    region: process.env.REACT_APP_REGION,
+    userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_USER_CLIENT_ID,
+  }
 })
 
 Amplify.addPluggable(new AWSIoTProvider({
-  aws_pubsub_region: 'ap-northeast-1',
-  aws_pubsub_endpoint: 'wss://a1rmwt2be3qlj4-ats.iot.ap-northeast-1.amazonaws.com/mqtt',
-  clientId: 'nigiyakashi_iot_thing'
+  aws_pubsub_region: process.env.REACT_APP_REGION,
+  aws_pubsub_endpoint: `wss://${process.env.REACT_APP_PUBSUB_ENDPOINT}/mqtt`,
+  clientId: cryptoRandomString({length: 10}),
 }))
 
-class App extends Component {
+const App = () => {
+  const [message, setMessage] = useState('')
+  const [topic, setTopic] = useState('')
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      messages: [],
-      topic: ''
-    }
-
-    this.handleSubscribe = this.handleSubscribe.bind(this)    
-    this.handlePublish = this.handlePublish.bind(this)
-    this.handleChangeTopic = this.handleChangeTopic.bind(this)
-    this.handleReceive = this.handleReceive.bind(this)
+  const handleChangeTopic = (event) => {
+    setTopic(event.target.value)
   }
- 
-  render() {
-    return (
-      <div className="App">
+
+  const handleChangeMessage = (event) => {
+    setMessage(event.target.value)
+  }
+
+  const handlePublish = () => {
+    PubSub.publish(topic, message)
+    console.log('send')
+  }
+
+  return (
+    <div className="App">
+      <div>
         <label>TOPIC:
-          <input type="text" onChange={this.handleChangeTopic}></input>
-        </label><br/>
-        <button onClick={this.handlePublish}>publish</button>
-        <button onClick={this.handleSubscribe}>subscribe</button>
-        <MessageList messages={this.state.messages} />
+          <input type="text" onChange={handleChangeTopic}></input>
+        </label>
       </div>
-    )
-  }
-
-  handlePublish(){
-    PubSub.publish(this.state.topic, 'pub message from browser!!')    
-  }
-  
-  handleSubscribe(topic){
-    PubSub.subscribe(this.state.topic).subscribe({
-      next: this.handleReceive,
-      error: error => console.error(error),
-      close: () => console.log('Done'),
-    })
-  }
-
-  handleChangeTopic(event) {
-    this.setState({topic: event.target.value})
-  }
-
-  handleReceive(msg){
-    const messages = this.state.messages
-    messages.push(JSON.stringify(msg.value))
-    this.setState({messages: messages})
-  }
+      <div>
+        <label>MESSAGE:
+          <input type="text" onChange={handleChangeMessage}></input>
+        </label>
+      </div>
+      <button onClick={handlePublish}>publish</button>
+    </div>
+  )
 }
- 
- 
-class MessageList extends React.Component {
- 
-  render() {
-    let id = 1
-    return (
-      <ul>
-        {this.props.messages.map(message => {
-          id += 1
-          return (
-           <li key={id}>
-             <div>
-               {message}
-             </div>
-           </li>
-         )
-       })}
-     </ul>
-    )
-  }
-}
- 
+
 export default withAuthenticator(App)
